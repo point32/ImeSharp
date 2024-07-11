cd22985a6ecd65c448412dfdf3a6a837a0e3bcf3
XNA代码同步(p0_INT32) <834936537@qq.com>
2024-07-11 21:40:05 +0800

    添加 Point.cs

---
+++ ImeSharp/TsfSharpStructures/Point.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace TsfSharp;
+[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+public struct Point
+{
+	public int X;
+
+	public int Y;
+}
